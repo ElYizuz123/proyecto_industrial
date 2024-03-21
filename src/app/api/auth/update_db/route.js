9e373c2d2d4b980378d0c07054c3a88f79c87cb1
José de Jesús Zárate García <jose.jesus.zarate.garcia@outlook.com>
2024-03-20 19:41:32 -0600

    update try

--- src/app/api/auth/update_db/route.js
+++ src/app/api/auth/update_db/route.js
@@ -5,10 +5,15 @@ export async function POST(request){
     const data = await request.json();
 
     console.log(data);
-    const user = await db.usuario.findMany({
+    const user = await db.usuario.update({
         where:{
-            user: data.user,
+            id: data.id,
         },
+        data:{
+            password: hashPass,
+            user: data.user,
+            almacen: data.warehouseNumber
+        }
     })
     const usuariosJSON = JSON.stringify(user);
     console.log(user)
--- src/app/users/read_where/page.jsx
+++ src/app/users/read_where/page.jsx
@@ -2,27 +2,48 @@
 import React, { useState } from 'react'
 
 const page = () => {
+    const [data, setData] = useState(null)
+    const handleOnChange = ({target:{name, value}}) => {
+        setData ({ ...data, [name]:value})
+    }
     const [users, setUsers] = useState(null)
-    const handleSubmit = (async () => {
-        const res = await fetch('/api/auth/read_where')
+    const handleSubmit = (async (e) => {
+        e.preventDefault();
+        const res = await fetch('/api/auth/read_where', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'aplication/json'
+            }
+        }) 
         const resJSON = await res.json()
         setUsers(JSON.parse(resJSON))
         console.log(resJSON)
     });
 
   return (
-    <div className='h-full h-full'>
-        <button onClick={handleSubmit} className='h-16 w-24 bg-emerald-300 m-3'>
-            <h1>Leer usuarios</h1>
-        </button>
-        <div className='h-48 w-48'>
-            {users && 
-            users.map((user)=> (<li key={user.id_usuario} className='p-3'>
-                <p>{user.user}</p>
-                <p>{user.password}</p>
-                </li>))}
-        </div>
-    </div>
+    <form className="flex flex-col items-center justify-center h-screen" onSubmit={handleSubmit}>
+            <div className="mb-4">
+                <label htmlFor="user" className="block">User:</label>
+                <input
+                    type="text"
+                    name="user"
+                    onChange={handleOnChange}
+                    className="border border-gray-400 rounded-md px-4 py-2 w-full text-black"
+                />
+            </div>
+            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">Submit</button>
+            <div className='h-48 w-48'>
+                    {users && 
+                    users.map((user)=> (<li key={user.id_usuario} className='p-3'>
+                        <p>{user.user}</p>
+                        <p>{user.password}</p>
+                        </li>))}
+            </div>
+        </form>
+        
+        
+    
   )
 }
 
